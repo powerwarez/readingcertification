181957b8fb92475ee2a5d1c4e6ee174a5bf5d335
powerwarez <powerware@naver.com>
2025-04-03 09:47:53 +0900

    server 삭제

--- netlify/functions/server.js
+++
@@ -1,8 +0,0 @@
-const { createRequestHandler } = require("@remix-run/netlify");
-const build = require("@remix-run/dev/server-build");
-
-// 여기가 중요 - process.env 대신 명시적 모드 사용
-exports.handler = createRequestHandler({
-  build,
-  mode: "production"
-});
